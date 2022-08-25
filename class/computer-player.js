b498755de6843562ab7259a32ce005c37a9307df
Michael An <huayu.michael@gmail.com>
2022-08-25 02:12:00 -0700

    attempted bonus

--- class/computer-player.js
+++ class/computer-player.js
@@ -2,8 +2,8 @@ class ComputerPlayer {
 	static getValidMoves(grid) {
 		let validMoves = [];
 
-		for (let row = 0; row < grid.length; row++) {
-			for (let col = 0; col < grid[0].length; col++) {
+		for (let row = 0; row <= 2; row++) {
+			for (let col = 0; col <= 2; col++) {
 				if (grid[row][col] === ' ') {
 					validMoves.push({ row: row, col: col });
 				}
@@ -21,28 +21,36 @@ class ComputerPlayer {
 		return validMoves[randomIndex];
 	}
 
-	// static getWinningMoves(grid, symbol) {}
+	static getWinningMoves(grid, symbol, symbolOther) {
+		let validMoves = this.getValidMoves(grid);
+
+		// if 1st to go, put it in the corner
+		if (validMoves.length === 9) {
+			return { row: 2, col: 2 };
+		}
+
+		// if 2nd to go or 2nd round, try to place move in middle, if can't, place across 1st move
+		if (validMoves.length === 8 || validMoves.length === 7) {
+			if (grid[1][1] === ' ') return { row: 1, col: 1 };
+			if (grid[1][1] === symbolOther) return { row: 0, col: 0 };
+		}
+	}
 
 	static getSmartMove(grid, symbol) {
 		// ! HA: why does this import have to be inside of this method, not in the global space?
 		const TTT = require('./ttt');
 
 		let validMoves = this.getValidMoves(grid);
+
 		let symbolOther = 'O';
 		if (symbol === 'O') symbolOther = 'X';
 
-		// check if ai is making the first move, put it in the corner
-		let countEmpty = 0;
-		for (const row of grid) {
-			for (const el of row) {
-				if (el === ' ') countEmpty++;
-			}
-		}
-		if (countEmpty === 9) {
-			return { row: 0, col: 0 };
-		}
+		// check winning moves first
+		let winningMove = this.getWinningMoves(grid, symbol, symbolOther);
+		if (winningMove !== undefined) return winningMove;
 
-		// test ai's symbol first then opponent's
+		// get smart moves
+		// test current symbol first and then opponent's
 		for (const each of [symbol, symbolOther]) {
 			// check each valid move
 			for (let i = 0; i < validMoves.length; i++) {
@@ -57,6 +65,15 @@ class ComputerPlayer {
 				grid[move.row][move.col] = ' ';
 			}
 		}
+
+		// try to put in corners
+		if (grid[0][0] === ' ') return { row: 0, col: 0 };
+		if (grid[2][2] === ' ') return { row: 2, col: 2 };
+		if (grid[0][2] === ' ') return { row: 0, col: 2 };
+		if (grid[2][0] === ' ') return { row: 2, col: 0 };
+
+		// try to put in middle
+		if (grid[1][1] === ' ') return { row: 1, col: 1 };
 
 		return ComputerPlayer.randomMove(grid);
 	}
